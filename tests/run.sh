a1210a5b04c0d717b71259989f5d7a6220eba4a5
Matthew Feickert <matthew.feickert@cern.ch>
2020-10-21 11:43:19 -0500

    Use installer in tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-mg5_DIR="/usr/local/MG5_aMC_v2_8_1"
-
-if [[ ! -d "${mg5_DIR}/PLUGIN/MG5aMC_PythonMEs" ]]; then
-  cp -r MG5aMC_PythonMEs "${mg5_DIR}/PLUGIN/"
-fi
+python install.py
 
 mg5_aMC --mode=MG5aMC_PythonMEs tests/test_MG5aMC_PythonMEs.mg5
 
